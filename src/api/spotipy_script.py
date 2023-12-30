48209554a2943c1ec9007cb9cb1db26bb8a927ae
Agata <aga.ala.k@gmail.com>
2023-12-30 21:42:42 +0100

    Add custom spotify session to omit request limit crash

--- src/api/spotipy_script.py
+++ src/api/spotipy_script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import requests, urllib3
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from datetime import datetime
@@ -15,12 +16,32 @@ client_secret = '0271f1939dc64b6ab8a8bec047d5a9a9'
 
 # Set up authentication using client credentials flow
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+session = requests.Session()
+
+retry = urllib3.Retry(
+    total=0,
+    connect=None,
+    read=0,
+    allowed_methods=frozenset(['GET', 'POST', 'PUT', 'DELETE']),
+    status=0,
+    backoff_factor=0.3,
+    status_forcelist=(429, 500, 502, 503, 504),
+    respect_retry_after_header=False  # <---
+)
+
+adapter = requests.adapters.HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+)
 
 # Playlist ID - Top 50 Polska
 playlist_id = '37i9dQZEVXbN6itCcaL3Tt'
 
-# Funkcja zapisuj¹ca informacje o utworach do pliku JSON
+# Funkcja zapisujï¿½ca informacje o utworach do pliku JSON
 def save_to_json(track_info_list, json_file_path):
     with open(json_file_path, 'w', encoding='utf-8') as file:
         json.dump(track_info_list, file, ensure_ascii=False, indent=4)
@@ -35,24 +56,24 @@ def load_existing_tracks(json_file_path):
 
 def retrive_track_info(playlist_id, frequency=None, json_file_path='nifi_in/songs.json'):
     try:
-        # Pobierz top 50 utworów z playlisty
+        # Pobierz top 50 utworï¿½w z playlisty
         top_tracks = sp.playlist_tracks(playlist_id, limit=50)
 
-        # SprawdŸ datê dodania pierwszego utworu z playlisty
+        # Sprawdï¿½ datï¿½ dodania pierwszego utworu z playlisty
         first_track_date_added = None
         if top_tracks['items']:
             first_track_date_added = datetime.strptime(top_tracks['items'][0]['added_at'], '%Y-%m-%dT%H:%M:%SZ')
 
-        # SprawdŸ, czy plik JSON istnieje
+        # Sprawdï¿½, czy plik JSON istnieje
         existing_tracks = load_existing_tracks(json_file_path)
 
-        # SprawdŸ, czy data dodania utworu istnieje w pliku JSON
+        # Sprawdï¿½, czy data dodania utworu istnieje w pliku JSON
         if existing_tracks is not None and 'date_added' in existing_tracks:
             last_track_date_added = existing_tracks['date_added'][-1]
             if first_track_date_added and first_track_date_added.date() == last_track_date_added.date():
                 return print("Brak nowych utworow do dodania")
 
-        # Przygotuj listê informacji o utworach do zapisu
+        # Przygotuj listï¿½ informacji o utworach do zapisu
         track_info_list = []
 
         for idx, track in enumerate(top_tracks['items'], start=1):
@@ -69,7 +90,7 @@ def retrive_track_info(playlist_id, frequency=None, json_file_path='nifi_in/song
         # Zapisz informacje o utworach do pliku JSON
         save_to_json(track_info_list, json_file_path)
 
-        # Wyœwietl informacje o utworach
+        # Wyï¿½wietl informacje o utworach
         for track_info in track_info_list:
             print(f"{track_info['rank']}. {track_info['track_name']} by {track_info['artist']} (Popularity: {track_info['popularity']})")
 
@@ -78,9 +99,9 @@ def retrive_track_info(playlist_id, frequency=None, json_file_path='nifi_in/song
     except Exception as e:
         print(f"Error: {e}")
 
-    # Czêstotliwoœæ wykonywania pêtli w sekundach
+    # Czï¿½stotliwoï¿½ï¿½ wykonywania pï¿½tli w sekundach
     # time.sleep(frequency)
     return
 
-# Pobieranie utworów do oddzielnej ramki dla danego dnia
+# Pobieranie utworï¿½w do oddzielnej ramki dla danego dnia
 retrive_track_info(playlist_id)
